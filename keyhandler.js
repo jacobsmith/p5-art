589c53d5360e0024de7cbb8074286da001540a10
Jacob Smith <jacob.wesley.smith@gmail.com>
2022-02-08 11:44:58 -0500

    add rotation and file select keyboard shortcuts

---
+++ keyhandler.js
@@ -0,0 +1,38 @@
+document.addEventListener('keydown', handleKeyPress);
+
+let rotateMode = false;
+let rotation = 0;
+
+function toggleRotateMode() {
+  rotateMode = !rotateMode;
+}
+
+function rotateModeHandle(e) {
+  if (e.key == 'ArrowRight') {
+    rotation = rotation + 0.1
+    document.body.style.transform = `rotate(${rotation}deg)`
+  } else if (e.key == 'ArrowLeft') {
+    rotation = rotation - 0.1
+    document.body.style.transform = `rotate(${rotation}deg)`
+  } else if (e.key == 'r') {
+    toggleRotateMode(); 
+  }
+}
+
+function handleKeyPress(e) {
+  if (rotateMode) {
+    return rotateModeHandle(e);
+  }
+
+  if (e.key == 'r') {
+    toggleRotateMode();
+  }
+  
+  if (e.key == 'ArrowRight') {
+    increaseFile();
+  } else if (e.key == 'ArrowLeft') {
+    decreaseFile();
+  } 
+
+  console.log(e);
+}

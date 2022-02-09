e7f506b47809d49bc4380f67bb7981a7601bfc1f
Jacob Smith <jacob.wesley.smith@gmail.com>
2022-02-08 21:13:07 -0500

    update pendulum kick

--- double-pendulum.js
+++ double-pendulum.js
@@ -2,8 +2,9 @@
 // https://thecodingtrain.com/CodingChallenges/093-double-pendulum.html
 // https://youtu.be/uWzPe_S-RVE
 
-let r1 = 125;
-let r2 = 125;
+// calculate arm length based on window height
+let r1 = null;
+let r2 = null;
 let m1 = 10;
 let m2 = 10;
 let a1 = 0;
@@ -16,11 +17,9 @@ let px2 = -1;
 let py2 = -1;
 let cx, cy;
 
-let buffer;
-
 function setup() {
-  r1 = random(120, 130);
-  console.log(r1);
+  let armWidth = (window.innerHeight / 4) - 10;
+  r1 = r2 = armWidth;
 
   var w = window.innerWidth;
   var h = window.innerHeight;  
@@ -31,9 +30,6 @@ function setup() {
   a2 = PI / 2;
   cx = width / 2;
   cy = height / 2;
-  buffer = createGraphics(width, height);
-  buffer.background(0);
-  buffer.translate(cx, cy);
 }
 
 let trail = [];
@@ -57,18 +53,14 @@ class Trail {
     let green = constrain(abs(50 - percentDecay) * 5, 0, 255);
 
     noFill();
-    stroke(red, green, green, alpha);
-    strokeWeight(2);
+    stroke(red, green, blue, alpha);
+    strokeWeight(8);
     line(this.x, this.y, nextPoint.x, nextPoint.y);
   }
 }
 
 function draw() {
   background(0);
-
-
-  imageMode(CORNER);
-  image(buffer, 0, 0, width, height);
 
   let num1 = -g * (2 * m1 + m2) * sin(a1);
   let num2 = -m2 * g * sin(a1 - 2 * a2);
@@ -94,23 +86,28 @@ function draw() {
   let x2 = x1 + r2 * sin(a2);
   let y2 = y1 + r2 * cos(a2);
 
-  // line(0, 0, x1, y1);
-  // fill(255);
-  // ellipse(x1, y1, m1, m1);
+  line(0, 0, x1, y1);
+  fill(255);
+  ellipse(x1, y1, m1, m1);
 
-  // line(x1, y1, x2, y2);
-  // fill(255);
-  // ellipse(x2, y2, m2, m2);
+  line(x1, y1, x2, y2);
+  fill(255);
+  ellipse(x2, y2, m2, m2);
  
   a1_v += a1_a;
   a2_v += a2_a;
   a1 += a1_v;
   a2 += a2_v;
 
+  if (random(0, 1000) > 998) {
+    a1_v *= 1.1
+    a2_v *= 1.1
+    console.log('kick')
+  }
+
   // a1_v *= 0.99;
   // a2_v *= 0.99;
 
-  buffer.stroke(255);
   if (frameCount > 1) {
     trail.push(new Trail(x2, y2))
   }
@@ -123,9 +120,6 @@ function draw() {
     let t = trail[i];
     t.draw(trail[i+1])
   }
-  // for (const t of trail) {
-  //   t.draw();
-  // }
 
   px2 = x2;
   py2 = y2;

65d46815c05965bc8ad4254e1fb606b193f0362e
Jacob Smith <jacob.wesley.smith@gmail.com>
2022-02-12 22:21:07 -0500

    add raindrop circle

---
+++ raindrop-circle.js
@@ -0,0 +1,63 @@
+const circles = [];
+let maxAge = 100;
+const NUM_CIRCLES = 50;
+const MAX_WIDTH = 60;
+
+function setup() {
+  var w = window.innerWidth;
+  var h = window.innerHeight;  
+  canvas=createCanvas(w, h);
+
+  for (i = 0; i < NUM_CIRCLES; i++) {
+    circles.push(new Circle());
+  }
+}
+
+function draw() {
+  background(0)
+
+  for (c of circles) {
+    c.update();
+    c.draw();
+  }
+}
+
+class Circle {
+  constructor() {
+    this.setup();
+  }
+
+  setup() {
+    this.x = random(MAX_WIDTH, width - MAX_WIDTH);
+    this.y = random(MAX_WIDTH, height - MAX_WIDTH);
+    this.r = random(5, MAX_WIDTH);
+    this.age = random(-maxAge, 0);
+  }
+
+  update() {
+    this.age += 1;
+    this.r += random(0.2, 0.8);
+
+    if (this.age > maxAge) {
+      this.setup();
+    }
+  }
+
+  draw() {
+    if (this.age < 0) { return; }
+    noFill();
+    const alpha = map(this.age, 0, 100, 128, 0, true);
+    stroke(255, alpha);
+    ellipse(this.x, this.y, this.r, this.r);
+  }
+}
+
+function keyPressed() {
+  if (keyCode == 38) {
+    // up
+    maxAge += 10;
+  } else if (keyCode == 40) {
+    // down
+    maxAge -= 10;
+  }
+}
